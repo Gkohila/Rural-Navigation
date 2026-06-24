83d04065734d6ea17085dea13dd17558b33f5a33
VijayapriyaV <vijayapriya1892003@gmail.com>
2026-06-24 14:39:12 +0530

    Added direction data model and updated OSRM service

---
+++ lib/models/direction_data.dart
@@ -0,0 +1,23 @@
+class DirectionData {
+
+  final String duration;
+
+  final String distance;
+
+  final String arrivalTime;
+
+  final List<Map<String, dynamic>> directions;
+
+  DirectionData({
+
+    required this.duration,
+
+    required this.distance,
+
+    required this.arrivalTime,
+
+    required this.directions,
+
+  });
+
+}
